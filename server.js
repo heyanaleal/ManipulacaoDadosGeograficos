6b1ae44e1527fd7f13fd9b559daa25f3454bf64a
Ana Clara Oliveira Leal <90358925+heyanaleal@users.noreply.github.com>
2023-05-21 21:34:29 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,17 +1,23 @@
+// server.js
 const express = require('express');
-const bodyParser = require('body-parser');
-const authRouter = require('./routes/authRouter');
-
 const app = express();
 
-app.use(bodyParser.json());
+// Importando os roteadores
+const v1Router = require('./routes/v1');
+const v2Router = require('./routes/v2');
+const v3Router = require('./routes/v3');
+const v4Router = require('./routes/v4');
 
-app.get('/', (req, res) => {
-  res.send('Bem-vindo à API GeoPoly!');
-});
+// Configurando middlewares
+app.use(express.json());
 
-app.use('/v1', authRouter);
+// Roteadores
+app.use('/v1', v1Router);
+app.use('/v2', v2Router);
+app.use('/v3', v3Router);
+app.use('/v4', v4Router);
 
+// Iniciando o servidor
 app.listen(3000, () => {
-  console.log('Servidor rodando na porta 3000');
+  console.log('Servidor iniciado na porta 3000');
 });
