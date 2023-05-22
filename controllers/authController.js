2b2050a7f531a2a33bbef16206f926364238c511
Ana Clara Oliveira Leal <90358925+heyanaleal@users.noreply.github.com>
2023-05-22 18:48:18 -0300

    adicionando formato de requisicao

--- controllers/authController.js
+++ controllers/authController.js
@@ -8,6 +8,12 @@ class AuthController {
   authenticate(req, res) {
     const { email, password } = req.body;
 
+
+    if (!email || !password) {
+      res.status(400).json({ message: 'Formato da requisição inválido' });
+      return;
+    }
+
     const isAuthenticated = this.authModel.authenticate(email, password);
 
     if (isAuthenticated) {
